f465dd27938f6119e62b96965f4b00d7212271ba
Le Dang Vu <ledangvu@gmail.com>
2021-09-11 19:44:34 +0700

    add webapp

--- build/dockerbuild.sh
+++ build/dockerbuild.sh
@@ -1,7 +1,14 @@
 git clone https://github.com/jerryno6/AKS_Sample.git
-
 git checkout develop
 
+# build for gprc
 cd src/Services/AKS_Sample.Services.GreetService
-
 docker build -t ledangvu/ledangvu_aks_grpc:latest .
+
+# build for webapi
+cd src/Services/WebApi
+docker build -t ledangvu/ledangvu_aks_webapi:latest .
+
+# build for webapp
+cd src/Services/WebApplication
+docker build -t ledangvu/ledangvu_aks_webapp:latest .
