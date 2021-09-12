7763400e6bb7421df5e19f6bdb4f4e057cec96e6
Le Dang Vu <ledangvu@gmail.com>
2021-09-12 19:32:40 +0700

    update for test deployment

--- deploy/Azure_AKS/deployaks.sh
+++ deploy/Azure_AKS/deployaks.sh
@@ -1,18 +1,21 @@
 #export LOCATION=southeastasia
-export RESOURCE_GROUP=learn-aa6abe3a-67e1-458a-95fd-2114f0163d62
+export RESOURCE_GROUP=learn-87364694-e797-4635-8031-77bd3ac1feba
 export CLUSTER_NAME=aks-contoso-video
 
 #create resources
 #az group create -n $RESOURCE_GROUP -l $LOCATION
+sleep 1m
 
 az aks create \
     --resource-group $RESOURCE_GROUP \
     --name $CLUSTER_NAME \
-    --node-count 1 \
+    --node-count 2 \
     --enable-addons http_application_routing \
     --generate-ssh-keys \
     --node-vm-size Standard_B2s \
     --network-plugin azure
+
+sleep 1m
 
 az aks nodepool add \
     --resource-group $RESOURCE_GROUP \
@@ -20,6 +23,8 @@ az aks nodepool add \
     --name userpool \
     --node-count 2 \
     --node-vm-size Standard_B2s
+
+sleep 1m
 
 # Link your Kubernetes cluster with kubectl
 az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP
