191a64952ab85b7faacf76a9956686db8fc20988
Le Dang Vu <ledangvu@gmail.com>
2021-10-03 10:16:46 +0700

    merge all type of deployment into 1 file

--- deploy/localkubernetes/kubctl deployment/localdeploy.sh
+++ deploy/localkubernetes/kubctl deployment/localdeploy.sh
@@ -1,9 +1,6 @@
 
 kubectl apply -f ./deployment.yaml
-kubectl apply -f ./nodeport.yaml
-
 kubectl wait --for=condition=available --timeout=600s deployment/contoso-website -n default
-
 
 kubectl get svc
 kubectl get pods
