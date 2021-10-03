cfe354379af2a491f6327ea86c0e910607331e33
Le Dang Vu <ledangvu@gmail.com>
2021-10-03 11:42:10 +0700

    we will create resource later

--- build/createresource.sh
+++
@@ -1,7 +0,0 @@
-export LOCATION=southeastasia
-export RESOURCE_GROUP=learn-04de4d4b-8b6e-49d4-b2f5-cf29151abeff
-export CLUSTER_NAME=aks-contoso-video
-
-#az group create -n $RESOURCE_GROUP -l $LOCATION
-
-az acr create -n VuleRegistry -g $RESOURCE_GROUP --sku Standard
