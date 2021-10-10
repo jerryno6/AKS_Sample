552ec1d02b2dcaeadf77a08f1ada56d5bceb4fc2
Le Dang Vu <ledangvu@gmail.com>
2021-10-10 22:13:39 +0700

    add commands for build and push image

--- build/dockerbuild.sh
+++ build/dockerbuild.sh
@@ -1,14 +1,29 @@
 git clone https://github.com/jerryno6/AKS_Sample.git
 git checkout develop
 
-# build for gprc
-cd src/Services/AKS_Sample.Services.GreetService
-docker build -t ledangvu/ledangvu_aks_grpc:latest .
+# build & push for webapp
+cd ./src/Services/WebApplication
+docker build -t ledangvu/ledangvu_aks_webapp:latest .
+docker push ledangvu/ledangvu_aks_webapp:latest
 
-# build for webapi
+# build & push for webapi
 cd src/Services/WebApi
 docker build -t ledangvu/ledangvu_aks_webapi:latest .
+docker push ledangvu/ledangvu_aks_webapi:latest
 
-# build for webapp
-cd src/Services/WebApplication
-docker build -t ledangvu/ledangvu_aks_webapp:latest .
+# build & push for  gprc
+cd src/Services/AKS_Sample.Services.GreetService
+docker build -t ledangvu/ledangvu_aks_grpc:latest .
+docker push ledangvu/ledangvu_aks_gprc:latest
+
+# push it to Azure  acr
+# you might need to login using #az acr login --name vule14registry
+docker tag ledangvu/ledangvu_aks_webapp:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapp:latest
+docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapp:latest
+
+docker tag ledangvu/ledangvu_aks_webapi:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapi:latest
+docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapi:latest
+
+docker tag ledangvu/ledangvu_aks_grpc:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_grpc:latest
+docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_grpc:latest
+
