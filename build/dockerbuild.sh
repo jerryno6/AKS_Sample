4ee08742faa5e085956ba251e9cae18efd4f2678
Le Dang Vu <vu.le.extern@karstadt.de>
2021-11-13 17:02:59 +0700

    build for node & reactapp

--- build/dockerbuild.sh
+++ build/dockerbuild.sh
@@ -27,12 +27,23 @@ docker build -t ledangvu/ledangvu_aks_grpc:latest .
 docker push ledangvu/ledangvu_aks_gprc:latest
 
 # push it to Azure  acr
+# you might need service principal credential to login acr, to reset credential, use command `az ad sp credential reset --name APP_ID``
+#{
+#  "appId": "7b8c8fc7-1246-4f8c-a54e-0f82ba1be14a",
+#  "displayName": "ServicePrincipalName",
+#  "name": "7b8c8fc7-1246-4f8c-a54e-0f82ba1be14a",
+#  "password": "BLv.yPKHSmuNNZadK4duUVUGPAbRDvNAAA",  -- the password has been changed
+#  "tenant": "61a34ea2-8a35-416e-af85-731e24572b82"
+#}
 # you might need to login using #az acr login --name vule14registry
 docker tag ledangvu/ledangvu_aks_webapp:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapp:latest
 docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapp:latest
 
 docker tag ledangvu/ledangvu_aks_webapp:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_reactapp:latest
 docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_reactapp:latest
+
+docker tag ledangvu/ledangvu_aks_webapp:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_nodeapi:latest
+docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_nodeapi:latest
 
 docker tag ledangvu/ledangvu_aks_webapi:latest vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapi:latest
 docker push vule14registry.azurecr.io/ledangvu/ledangvu_aks_webapi:latest
